ed8de9fb228ec086c31100514240a2ec180cfec7
Xi Wang <xiwangsxw@gmail.com>
2020-06-13 03:42:32 -0400

    added typewriter animation

--- MeowNoodle/Dialog.gd
+++ MeowNoodle/Dialog.gd
@@ -1,25 +1,92 @@
 extends TextureRect
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
+var currentLine = 0
+var script1 = [
+	"Good morning dad!",
+	"Oh hi, Hannah.",
+	"Was that the landlord that just left?",
+	"Yeah, that’s him.",
+	"What did he say?",
+	"Don’t worry about it Hannah. Why are you here?",
+	"Oh, mom wants me to check on you and see if there’s anything I can help with. After all, this is the first day of opening since the long pandemic!",
+	"Haha, yes, I’m glad that’s over now.",
+	"Is everything ok, dad? You look… worried.",
+	"It’s nothing, we’ll be fine...",
+	"No, tell me dad. I know that look on your face, is that anything to do with the landlord?",
+	"Okay… Hannah, there’s something you need to know. But don’t tell your mother now, she needs to recuperate in peace.",
+	"I won’t.",
+	"We… we might lose our store.",
+	"What?! ",
+	"Yes. The landlord asked me to pay for rent from the past two months. I’m afraid we don’t have that much savings now, and not even enough to hire new workers after we spend so much on...",
+	"…Yes, dad, I know, on mom’s hospitalization. Dad, you know, I can help too. ",
+	"Hannah… I will work my hardest to keep you guys safe and warm. You don’t need to do this.",
+	"But I want to. You and mom always worked so hard just so I could choose for my own life. Now It’s time for me to take responsibility and help out our family. I've made my decision.",
+	"Hannah. You’ve grown up. We will do this together.",
+	"Yes. We can overcome this!"]
+# 
 func _ready():
 	change_name("hannah")
-	$Dialog.text = ("Hi dad! What are you doing here?")
-	 # Replace with function body.
-
+	$Dialog/AnimationPlayer.play("typewriter_effect")
+	$Dialog.text = (script1[currentLine])
+	
+		
+func _process(delta):
+#	print charater name fucntion
+	if currentLine % 2 == 0:
+		change_name("hannah")
+	else:
+		change_name("dad")
+		
 func change_name(name):
+#	print charater name
 	if name == "hannah":
-		$CharacterName.text = ("Hannah: ")
+		$CharacterName.text = ("- Hannah -")
 	elif name == "dad":
-		$CharacterName.text = ("Dad: ")
+		$CharacterName.text = ("- Dad -")
+			
+func _on_TextureButton_pressed():
+	currentLine += 1
+	$Dialog.text = (script1[currentLine])
+	# change typewriter animation effect speed
+	if $Dialog.text.length() < 15:
+		$Dialog/AnimationPlayer.playback_speed = 2.5
+		$Dialog/AnimationPlayer.play("typewriter_effect")
+	elif $Dialog.text.length() > 15 && $Dialog.text.length() < 40:
+		$Dialog/AnimationPlayer.playback_speed = 2
+		$Dialog/AnimationPlayer.play("typewriter_effect")
+	else:
+		$Dialog/AnimationPlayer.playback_speed = 1
+		$Dialog/AnimationPlayer.play("typewriter_effect")
+	print($Dialog.text.length())
+	print($Dialog/AnimationPlayer.playback_speed)
+	
+	
 
-func dialog(line):
 
-	pass
-#func _process(delta):
-#pass
+
+
+	
+#Dad: Oh hi Hannah.
+#Hannah: Was that the landlord that just left?
+#Dad: … Yeah, that’s him.
+#Hannah: What did he say?
+#Dad: Don’t worry about it Hannah. Why are you here?
+#Hannah: Oh, mom wants me to check on you and see if there’s anything I can help with. After all, this is the first day of opening since the long pandemic!
+#Dad: Haha, yes, I’m glad that’s over now.
+#Hannah: Is everything ok, dad? You look… worried.
+#Dad: It’s nothing, we’ll be fine...
+#Hannah: No, tell me dad. I know that look on your face, is that anything to do with the landlord?
+#Dad: Okay… Hannah, there’s something you need to know. But don’t tell your mother now, she needs to recuperate in peace.
+#Hannah: I won’t.
+#Dad: We… we might lose our store.
+#Hannah: What?! 
+#Dad: Yes. The landlord asked me to pay for rent from the past two months. I’m afraid we don’t have that much savings now, and not even enough to hire new workers after we spend so much on...
+#Hannah: …Yes, dad, I know, on mom’s hospitalization. Dad, you know, I can help too. 
+#Dad: Hannah… I will work my hardest to keep you guys safe and warm. You don’t need to do this.
+#Hannah: But I want to. You and mom always worked so hard just so I could choose for my own life. Now It’s time for me to take responsibility and help out our family. I've made my decision.
+#Dad: Hannah. You’ve grown up. We will do this together.
+#Hannah: Yes. We will overcome this!
+#
+
+
+
